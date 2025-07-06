7ac2228cc06488e4665e7772221117c1eeff41ca
Shishamo <shishamodesu@outlook.jp>
2025-07-06 16:03:30 +0900

    Fix: Remove unnecessary Includes

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 
 #include <fstream>
 #include <iostream>
-#include <random>
 #include <string>
 
 //TODO: UTF-8 support.
--- main.hpp
+++ main.hpp
@@ -4,7 +4,6 @@
 
 #include <fstream>
 #include <iostream>
-#include <random>
 #include <string>
 
 namespace easy_encryption
